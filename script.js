58383af45b61b66c35ff091c8ad1cc9b302963d8
aliamk <alia.azarian@gmail.com>
2020-07-16 00:16:22 +0100

    added player controls in html, css and functionality in JS

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-@import url('https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap');
+
+const music = document.querySelector('audio')
+const prevBtn = document.getElementById('prev')
+const playBtn = document.getElementById('play')
+const nextBtn = document.getElementById('next')
+
+
+// Check of playing
+let isPlaying = false
+
+// Play
+function playSong() {
+  isPlaying = true
+  playBtn.classList.replace('fa-play', 'fa-pause')
+  playBtn.setAttribute('title', 'Pause')
+  music.play()
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false
+  playBtn.classList.replace('fa-pause', 'fa-play')
+  playBtn.setAttribute('title', 'Play')
+  music.pause()
+}
+
+//  Play/Pause Event Listener
+playBtn.addEventListener('click', () => ( 
+  isPlaying ? pauseSong() : playSong()
+))
